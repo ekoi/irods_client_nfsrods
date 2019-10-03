ab7ded99915dd51bc821a7631591f8b4c1af1a90
ekoi <eko.indarto@dans.knaw.nl>
2019-10-03 21:00:21 +0200

    intermediate commit

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-java -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -jar /opt/irods-clients/nfsrods/nfs4j-irodsvfs-pom-1.0.0-SNAPSHOT.jar
+java -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -jar /opt/irods-clients/nfsrods/nfsrods-1.0.0-SNAPSHOT-jar-with-dependencies.jar
